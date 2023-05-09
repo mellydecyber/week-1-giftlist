5de779912b9085ef0948f6b95c5e1d062ee9ee4a
Melli Tiara <melly.tiara92@gmail.com>
2023-05-09 22:48:26 +0700

    first commit

--- server/index.js
+++ server/index.js
@@ -8,15 +8,16 @@ app.use(express.json());
 
 // TODO: hardcode a merkle root here representing the whole nice list
 // paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+const MERKLE_ROOT = '59cdac30b81c75221fe13f48ab1a09cc8c6edf3e1e3f24b1143dd6ac0c9e661c';
 
 app.post('/gift', (req, res) => {
   // grab the parameters from the front-end here
   const body = req.body;
 
   // TODO: prove that a name is in the list 
-  const isInTheList = false;
-  if(isInTheList) {
+  const isInTheList = verifyProof(body.proof, body.leaf, MERKLE_ROOT);
+ 
+  if (isInTheList) {
     res.send("You got a toy robot!");
   }
   else {
